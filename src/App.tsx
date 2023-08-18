167a22cef90909ed9a8a8faa47879a8514a86d4c
gretoq <dovhun3d@gmail.com>
2023-08-18 10:39:00 +0300

    build: create react app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss';
+
+import React from 'react';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <>
+      <h1>Your app here</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </>
+  );
+}
+
+export default App;
